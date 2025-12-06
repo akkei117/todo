a8976d30702a1f488c9d8efea1f51d58a001c087
akkei117 <malviyasanidhya17@gmail.com>
2025-12-06 18:44:17 +0530

    this is the first prototype
    in this prototype currently the ui is ready but the logic is not working

---
+++ lib/components/todotile.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Todotile extends StatefulWidget {
+  final String taskname;
+  final bool taskcompleted;
+  final void Function(bool?)? onChanged;
+
+  const Todotile({
+    super.key,
+    required this.onChanged,
+    required this.taskcompleted,
+    required this.taskname,
+  });
+
+  @override
+  State<Todotile> createState() => _TodotileState();
+}
+
+class _TodotileState extends State<Todotile> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+        color: Colors.yellow,
+        child: Row(
+          children: [
+            //Check box
+            Checkbox(value: widget.taskcompleted, onChanged: widget.onChanged),
+
+            //Text (Task)
+            Padding(
+              padding: EdgeInsetsGeometry.all(25),
+              child: Text(widget.taskname),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/homepage.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:todo/components/todotile.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  List todoList = [
+    ["Make the OS notes", false],
+    ["Play badminton", false],
+    ["Play badminton", true],
+  ];
+
+  void checkChange(bool? value, int index) {
+    setState(() {
+      todoList[index][1] = !todoList[index][1];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        centerTitle: true,
+        title: Text(
+          "To-Do",
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.black,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return Todotile(
+            onChanged: (value) => checkChange,
+            taskcompleted: todoList[index][1],
+            taskname: todoList[index][0],
+          );
+        },
+      ),
+    );
+  }
+}
